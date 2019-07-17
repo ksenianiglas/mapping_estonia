2b98bd33c7b2296900a367ca674d8edf226a9c7e
Ksenia <50526165+ksenianiglas@users.noreply.github.com>
2019-07-17 13:51:48 +0300

    Add files via upload

---
+++ roads.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+fig = plt.figure(figsize=(20,20))
+
+#initiate a baemap layer
+m = Basemap(
+    resolution = 'l',
+    projection = 'merc',
+    #coordinates of the corners - lower left lat/lon and upper right lat/lon
+    llcrnrlat=57.4,llcrnrlon=21.5,
+           urcrnrlat=59.8,urcrnrlon=28.5);
+
+#fill in the background
+m.drawmapboundary(fill_color='#1c1c1c')
+m.fillcontinents(color='#1c1c1c',lake_color='#1c1c1c');
+
+#I downloaded the .shp.zip archive for Estonia from http://download.geofabrik.de/
+#and placed the files in a subfolder "geo"
+m.readshapefile('geo/gis_osm_roads_free_1', 'roads', drawbounds = True,
+                color='#b8b8b8')
+plt.show()
+plt.savefig('estonian_roads')
